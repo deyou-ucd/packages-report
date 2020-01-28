c813e3f9e57ed1ffd845fcb3f782c26cd4ee8131
Dominique Young <deyou@ucdavis.edu>
2020-01-27 16:07:28 -0800

    Add R script, .Rpoj file, and .gitignore.

---
+++ write-installed-packages.R
@@ -0,0 +1,34 @@
+## deja vu from earlier!
+
+library("tidyverse")
+
+## create a data frame of your installed packages
+## hint: installed.packages() is the function you need
+
+pkg <- data.frame(installed.packages())
+
+# Kara suggested
+pkgs <- as_tibble(installed.packages())
+
+## optional: select just some of the variables, such as
+##   * Package
+##   * LibPath
+##   * Version
+##   * Priority
+##   * Built
+
+pkgfew <- pkg %>%
+  dplyr::select(Package, LibPath, Version, Priority, Built)
+
+#Look at my data to make sure it looks good.
+
+## write this data frame to data/installed-packages.csv
+## hint: readr::write_csv() or write.table()
+## idea: try using here::here() to create the file path
+
+library("here")
+write_csv(pkgfew, here("data", "installed-packages.csv"))
+
+## YES overwrite the file that is there now (or delete it first)
+## that's a old result from me (Jenny)
+## it an example of what yours should look like and where it should go
